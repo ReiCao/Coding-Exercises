8bde1a52e8dc3c9343dc648b81ecda0a02d40a46
ReiCao <jb.cavatti@gmail.com>
2026-02-01 22:55:10 -0300

    Java problems
    the first 2 java problems

---
+++ BeeCrowd - Java/Problem_1001.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner userInput = new Scanner(System.in);
+        int A = userInput.nextInt();
+        int B = userInput.nextInt();
+        userInput.close();
+        
+        int result = A + B;
+        
+        System.out.println("X = " + result);
+    }
+}
